394b2ae950b5fa2e26da5f522b33a724f8264747
Serhii Borulko <borulkosergey@icloud.com>
2019-12-18 10:50:25 +0200

    Add Abstract middleware. All user-defined middleware should extend it

---
+++ Classes/Middleware/Api/AbstractRouteMiddleware.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types = 1);
+
+namespace LMS\Routes\Middleware\Api;
+
+/* * *************************************************************
+ *
+ *  Copyright notice
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+use Psr\Http\Message\ServerRequestInterface;
+use LMS\Facade\Extbase\TypoScriptConfiguration;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+
+/**
+ * @author Sergey Borulko <borulkosergey@icloud.com>
+ */
+abstract class AbstractRouteMiddleware
+{
+    /**
+     * @var \Psr\Http\Message\ServerRequestInterface $request
+     */
+    private $request;
+
+    /**
+     * @var array
+     */
+    private $properties;
+
+    /**
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param array                                    $properties
+     */
+    public function __construct(ServerRequestInterface $request, array $properties)
+    {
+        $this->request = $request;
+        $this->properties = $properties;
+    }
+
+    /**
+     * @throws \Symfony\Component\Routing\Exception\MethodNotAllowedException
+     */
+    abstract public function process(): void;
+
+    /**
+     * @param string $message
+     *
+     * @throws \Symfony\Component\Routing\Exception\MethodNotAllowedException
+     */
+    protected function deny(string $message): void
+    {
+        throw new MethodNotAllowedException([], $message);
+    }
+
+    /**
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    protected function getRequest(): ServerRequestInterface
+    {
+        return $this->request;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getProperties(): array
+    {
+        return $this->properties;
+    }
+
+    /**
+     * @param string $extKey
+     *
+     * @return array
+     */
+    protected function getSettings(string $extKey): array
+    {
+        return TypoScriptConfiguration::getSettings($extKey) ?: [];
+    }
+}
