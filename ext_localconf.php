cb5ec42224dcca91b323563871236433b936a44e
Serhii Borulko <borulkosergey@icloud.com>
2019-12-18 10:48:48 +0200

    Use TYPO3 caching system.

--- ext_localconf.php
+++ ext_localconf.php
@@ -26,3 +26,7 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_routes'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_routes'] = [];
+}
