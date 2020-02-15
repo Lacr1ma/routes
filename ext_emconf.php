1e4625e678fa3d532aab285177e9f1e9070889b2
Serhii Borulko <borulkosergey@icloud.com>
2020-02-15 18:24:05 +0200

    [TASK] Up the version to 1.8.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['routes'] = [
     'title' => 'Extbase Yaml Routes',
     'description' => 'Provides an ability to bind a route slug to the certain Extbase Action endpoint.',
     'category' => 'fe',
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'borulkosergey@icloud.com',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '1.8.5',
+    'version' => '1.8.6',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99'
